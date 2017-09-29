d5187297ea745ea8c641bfa21a33202fad562364
xrr2016 <xiaoranran1993@163.com>
2017-09-29 10:45:38 +0800

    :zap:

---
+++ 2/index.js
@@ -0,0 +1,17 @@
+class RandomWord {
+  constructor () {
+    this.letters = ['a', 'b', 'c', 'd', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+  }
+  newWord () {
+    let word = ''
+    let len = Math.floor(Math.random() * 12)
+    while (word.length < len) {
+      word += this.letters[Math.floor(Math.random() * 26)]
+    }
+    return word
+  }
+}
+
+const word = new RandomWord()
+
+console.log(word.newWord())
